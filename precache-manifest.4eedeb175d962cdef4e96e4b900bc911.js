6a727e77bb948d5a0c349bd67289718424f8323e
Osvaldo Arzate <osvarz.new@gmail.com>
2020-08-10 12:32:32 -0500

    Updates

---
+++ precache-manifest.4eedeb175d962cdef4e96e4b900bc911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df17b2d3f66eab3f64a76cce7420a8b",
+    "url": "/frontendExerciseReact04/index.html"
+  },
+  {
+    "revision": "ffeca7fc2e61bb5b0c15",
+    "url": "/frontendExerciseReact04/static/css/main.e0656427.chunk.css"
+  },
+  {
+    "revision": "2088b773080c3d7291df",
+    "url": "/frontendExerciseReact04/static/js/2.05840ac8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontendExerciseReact04/static/js/2.05840ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffeca7fc2e61bb5b0c15",
+    "url": "/frontendExerciseReact04/static/js/main.fede66cf.chunk.js"
+  },
+  {
+    "revision": "b82538401eaad976a190",
+    "url": "/frontendExerciseReact04/static/js/runtime-main.a98ea115.js"
+  }
+]);
